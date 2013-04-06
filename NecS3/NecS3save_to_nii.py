01d91838aedb931658051ca846f7328f163e7223
Firas Moosvi <firas@moosvi.com>
2013-04-05 20:46:47 -0700

    fixed:	new location of json file.
    added: 	code to read in masks

--- NecS3/NecS3save_to_nii.py
+++ NecS3/NecS3save_to_nii.py
@@ -8,25 +8,44 @@ import sarpy
 import numpy
 import sarpy.fmoosvi.wrappers
 import json
-
+import nibabel
 
 ## Script to save IR-RARE files as niftis
 
 
-with open('/Volumes/Data/Dropboxes/PhD./Dropbox/Studies/NecS3/NecS3.json','r') as infile:
+with open('/Volumes/Data/Dropboxes/PhD./Dropbox/Studies/analysis/NecS3/NecS3.json','r') as infile:
     master_sheet = json.load(infile)
 
 NecS3 = sarpy.Experiment('NecS3').find_scan_by_protocol('04')
 
+################################
+# This is to export images for ROIs
+################################
+
+#for k,v in master_sheet.iteritems():
+#    
+#    try:
+#        name = k+'-1'
+#        
+#        sarpy.Scan(master_sheet[k]['24h-IR_A'][0]).pdata[0].export2nii(name)
+#        
+#    except:
+#        print k
+
+
+################################################################
+# This is to load them in after ROIs have been drawn using ImageJ
+################################################################
+
 for k,v in master_sheet.iteritems():
-    
-    try:
-        name1 = k+'-1'
-        name2 = k+'-2'        
+   
+    try: 
+        name = k+'-2'+'.nii'
+       
+        scan = sarpy.Scan(master_sheet[k]['24h-IR_A'][0])
+        roi = nibabel.load(name)
         
-        sarpy.Scan(master_sheet[k]['0h-IR_A'][0]).pdata[0].export2nii(name1)
-        sarpy.Scan(master_sheet[k]['24h-IR_A'][0]).pdata[0].export2nii(name2)
-        
+        scan.store_adata(key='IR_tumour_rois', data = roi)
+
     except:
         print k
-    
