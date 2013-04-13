d38107541a4a156805fb2c0d991e3a7437b55a68
Firas Moosvi <firas@moosvi.com>
2013-04-13 01:04:36 -0700

    fixes issue#1

--- NecS3/NecS3_LL_roi_flattened.py
+++ NecS3/NecS3_LL_roi_flattened.py
@@ -37,7 +37,8 @@ for k,v in master_sheet.iteritems():
         key_list.append('24h-IR_A') #5
         key_list.append('T1map_LL') #6
         key_list.append('IR_tumour_rois') #7
-    
+
+
 #        LL_1 = sarpy.Scan(master_sheet[k][key_list[2]][0]).adata[key_list[6]]
 #        LL_2 = sarpy.Scan(master_sheet[k][key_list[3]][0]).adata[key_list[6]]
 
@@ -55,9 +56,23 @@ for k,v in master_sheet.iteritems():
 
         T1_vals_day1.append( curr_T1s1[numpy.isfinite(curr_T1s1)] )
         T1_vals_day2.append( curr_T1s2[numpy.isfinite(curr_T1s2)] )
+#        
+#        if k == 'NecS3Hs11':  #debuggimg please ignore 
+#            pylab.figure()
+#            bins = numpy.linspace(200,3500,50)
+#            pylab.hist(T1_vals_day2[-1], bins, alpha=0.6)
+#            continue
+    
         
-    except:
-        pylab.close('all')
+#        print k
+#        pylab.figure()
+#        bins = numpy.linspace(200,3500,50)
+#        pylab.hist(T1_vals_day2[-1], bins, alpha=0.6)
+#        pylab.title(k)
+
+        
+    except KeyError:
+        #pylab.close('all')
         print('Expected error, please ignore {0}'.format(k))
         
 # Now squash the lists together, and turn into an array
@@ -69,14 +84,22 @@ T1_vals_day2 = [item for sublist in T1_vals_day2 for item in sublist]
 T1_vals_day2 = numpy.array(T1_vals_day2)
 
 
-
-bins = numpy.linspace(200,3500,50)
         
-pylab.figure()        
+bins = numpy.linspace(200,3500,50)
 pylab.hist(T1_vals_day1, bins, alpha=0.6)
 pylab.hist(T1_vals_day2, bins, alpha=0.6)
-        
-        
-        
-        
-        
+
+#bins = numpy.linspace(200,3500,50)
+#        
+#pylab.figure()        
+#pylab.hist(T1_vals_day1, bins, alpha=0.6)
+#pylab.hist(T1_vals_day2, bins, alpha=0.6)
+#        
+#        
+#        
+#b=pylab.figure()
+#a=pylab.imshow(necs3hs10.adata['T1map_LL'].data[:,:,3])
+#a.set_clim([500,3500])
+#pylab.colorbar()
+#b.canvas.draw()
+#        
