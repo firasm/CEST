bd28e4c9f5dfefb4c0f2a4592f24322b80769181
Firas Moosvi <firas@moosvi.com>
2015-03-29 13:21:38 -0700

    module name update, adding comments, fixed bugs

--- cest.py
+++ cest.py
@@ -7,20 +7,23 @@ from __future__ import division
 import numpy
 import sarpy
 import sarpy.fmoosvi.analysis
+import scipy
 
 
 ####### Functions #######
 
 def h_zspectrum_N(params,freqs):
     
-    arr = empty_like(freqs)*0
+    arr = numpy.empty_like(freqs)*0
     shift =  params[0]
     
     for i in numpy.arange(0,len(params[1:]),3):
         
-        A = params[i+1]
-        w0 = params[i+2]
-        lw =  params[i+3]
+        A = params[i+1] # Amplitude of the peak
+        w0 = params[i+2] # Resonant frequency of sample
+        lw =  params[i+3] # Line width (in units of freqs)
+
+        # Construct the multiple lorentzians added together
         tmp = numpy.divide(A,(1+4*((freqs-w0)/lw)**2))
         
         arr = arr+tmp
@@ -32,30 +35,29 @@ def h_residual_Zspectrum_N(params, y_data, w):
 
 def fit_water_peak(data,offset_freqs):
 
-	"""
-	Fits a Lorentzian to the data, and 
-	returns the water offset frequency
+    """
+    Fits a Lorentzian to the data, and 
+    returns the water offset frequency
 
-	"""
+    """
 
-	# Presumably the maximum signal will be at 
-	# some large frequency offset, so let's just
-	# use that as our baseline parameter
+    # Presumably the maximum signal will be at 
+    # some large frequency offset, so let's just
+    # use that as our baseline parameter
 
-	# Also, normalize the data so the fit is easier
+    # Also, normalize the data so the fit is easier
 
-	data = data / numpy.max(data)
+    data = data / numpy.max(data)
 
-	params_passed = [numpy.max(data),-1.,0.1,0.6]
+    params_passed = [numpy.max(data),-1.,0.1,0.6]
 
-	fit_params,cov,\
-	infodict,mesg,ier = scipy.optimize.leastsq(h_residual_Zspectrum_N,
-                                               params_passed,
-                                               args=(data, offset_freqs), 
-                                               full_output = False,
-                                               maxfev = 200)
+    fit_params,cov,infodict,mesg,ier = scipy.optimize.leastsq(h_residual_Zspectrum_N,
+        params_passed,
+        args=(data, offset_freqs),
+        full_output = True,
+        maxfev = 200)
 
-	return fit_params[1]
+    return fit_params[2]
 
 def shift_water_peak(scn_to_analyse=None, 
                      bbox = None,
@@ -75,6 +77,8 @@ def shift_water_peak(scn_to_analyse=None,
     :param bbox: bounding bbox
     :return: array with shifted peak positions
     """ 
+
+    scn = sarpy.Scan(scn_to_analyse)
 
     # Size info
     x_size = scn.pdata[pdata_num].data.shape[0]
@@ -104,13 +108,13 @@ def shift_water_peak(scn_to_analyse=None,
     bbox_mask = numpy.tile(bbox_mask.reshape(x_size,y_size,1),offsets) 
 
     # Fit the water peak pixel by pixel
-    B0_map = numpy.nan + numpy.empty( [x_size,y_size] )
+    B0_map = numpy.nan + numpy.empty([x_size,y_size])
 
+    dat = scn.pdata[0].data
     for x in xrange(bbox[0],bbox[1]):
         for y in xrange(bbox[2],bbox[3]):
 
-    		B0_map[x,y] = fit_water_peak(dat[x,y,:], 
-    					   				 freq_list)
+            B0_map[x,y] = fit_water_peak(dat[x,y],freq_list)
 
 
     return B0_map
