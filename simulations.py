e742879224f0c1a4a271794a970e7b17bfc9aceb
srveale <srveale42@gmail.com>
2015-05-08 13:42:49 -0700

    Added rotation functions and free precession function

---
+++ simulations.py
@@ -0,0 +1,48 @@
+##Collection of code to simulate CEST sequences
+
+import numpy as *
+def xrot(phi):
+    return numpy.array([[1,0,0,0,0,0,0],
+                        [0,1,0,0,0,0,0],
+                        [0,0,cos(phi),0,sin(phi),0,0],
+                        [0,0,0,cos(phi),0,sin(phi),0],
+                        [0,0,-sin(phi),0,cos(phi),0,0],
+                        [0,0,0,-sin(phi),0,cos(phi),0],
+                        [0,0,0,0,0,0,0],])
+
+def yrot(phi):
+    return numpy.array([[cos(phi),0,0,0,-sin(phi),0,0],
+                        [0,cos(phi),0,0,0,-sin(phi),0],
+                        [0,0,1,0,0,0,0],
+                        [0,0,0,1,0,0,0],
+                        [sin(phi),0,0,0,cos(phi),0,0],
+                        [0,sin(phi),0,0,0,cos(phi),0],
+                        [0,0,0,0,0,0,0]])
+
+def zrot(phi):
+    return numpy.array([[numpy.cos(phi), 0, numpy.sin(phi), 0, 0, 0, 0],
+                        [0, numpy.cos(phi), 0, numpy.sin(phi), 0, 0, 0],
+                        [-numpy.sin(phi), 0, numpy.cos(phi), 0, 0, 0, 0],
+                        [0, -numpy.sin(phi), 0, numpy.cos(phi), 1, 0, 0],
+                        [0, 0, 0, 0, 1, 0, 0],
+                        [0, 0, 0, 0, 0, 1, 0],
+                        [0, 0, 0, 0, 0, 0, 0],])
+
+def freeprecessTwoPool(dt, T1a=numpy.inf, T2a=numpy.inf, T1b = numpy.inf, T2b = numpy.inf, M0a=1000, M0b = 1.0, domega=0):
+    ''' return the A matrix and B vector for the dM/dt magnetization evolution '''
+    phi = domega*dt	 # Resonant precession, radians.
+    E1a = numpy.exp(-dt/T1a)
+    E1b = numpy.exp(-dt/T1b)
+    E2a = numpy.exp(-dt/T2a)
+    E2b = numpy.exp(-dt/T2b)
+    
+    B = numpy.array([0, 0, 0, 0, M0a*(1. - E1a), M0b*(1. - E1b), 0])
+
+    A = numpy.array([[E2a, 0, 0, 0, 0, 0, 0],
+                     [0, E2b, 0, 0, 0, 0, 0],
+                     [0, 0, E2a, 0, 0, 0, 0],
+                     [0, 0, 0, E2b, 0, 0, 0],
+                     [0, 0, 0, 0, E1a, 0, 0],
+                     [0, 0, 0, 0, 0, E1b, 0],
+                     [0, 0, 0, 0, 0, 0, 0 ]])
+    return numpy.dot(A, zrot(phi)),B
